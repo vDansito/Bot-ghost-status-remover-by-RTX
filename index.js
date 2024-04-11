7cd8be4e1d8cc3de1bb77c6c7f2509bff21c0236
vDansito <166733164+vDansito@users.noreply.github.com>
2024-04-11 17:08:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Viendo los Teletubbies", "Quiero a Tinkiwinki"];
 
 
 let currentIndex = 0;
